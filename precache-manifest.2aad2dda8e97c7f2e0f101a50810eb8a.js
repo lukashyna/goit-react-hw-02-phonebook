775a81d2cc994e2c9b92dcf9d23ee110194183c5
“Anastasiia <L_A_A_93@mail.ru>
2020-05-05 15:12:40 +0300

    Updates

---
+++ precache-manifest.2aad2dda8e97c7f2e0f101a50810eb8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "646b3bdebfc4c35a85b3a515dcbc46ef",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "a9f4b09931aaf19d52a2",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.88e33e26.chunk.css"
+  },
+  {
+    "revision": "cd36359184a09b95ab55",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.84760093.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.84760093.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9f4b09931aaf19d52a2",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.17574307.chunk.js"
+  },
+  {
+    "revision": "494cb0e9145d47a41a1a",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.c77db088.js"
+  }
+]);
